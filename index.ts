bec8b04f5155cccd8c1867c1bdde8a0bd06a6c73
letsget23 <letsget23@gmail.com>
2019-07-13 03:19:02 +0900

    include 테스트 통과

--- index.ts
+++ index.ts
@@ -1,9 +1,44 @@
 import { IItem, IQuery } from "./type";
 
+function clone(obj: object) {
+  if (obj === null || typeof obj !== "object") return obj;
+
+  var copy = obj.constructor();
+
+  for (var attr in obj) {
+    if (obj.hasOwnProperty(attr)) {
+      // @ts-ignore
+      copy[attr] = obj[attr];
+    }
+  }
+
+  return copy;
+}
+
 export const getResult = (items: IItem[], query: IQuery): IItem[] => {
-  return [];
+  let filteredItems: IItem[] = clone(items);
+
+  query.include.forEach(includeCond => {
+    const [key, value] = includeCond.split(":");
+
+    filteredItems = filteredItems.filter(item => {
+      return item[key as keyof (IItem)].toString() === value;
+    });
+  });
+
+  // TODO: exclude 기능
+
+  // TODO: sort 기능
+
+  return filteredItems;
 };
 
 export const getResultList = (items: IItem[], queries: IQuery[]): IItem[][] => {
-  return [];
+  let resultList: IItem[][] = [];
+
+  queries.forEach(query => {
+    resultList.push(getResult(items, query));
+  });
+
+  return resultList;
 };
