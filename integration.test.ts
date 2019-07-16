8a8150bb35104027c0d922c6ed9abd8d54d1a99f
letsget23 <letsget23@gmail.com>
2019-07-16 22:27:33 +0900

    통합 테스트 추가

---
+++ integration.test.ts
@@ -0,0 +1,20 @@
+import { items, queries } from "./dataset";
+import { getResultList } from ".";
+
+describe("integration test", () => {
+  it("should return correct result", () => {
+    const result = getResultList(items, queries);
+
+    expect(result.length).toBe(queries.length);
+
+    // 개발자가 결과를 알고있음. dataset.ts 참고
+    expect(result[0].length).toBe(3);
+    expect(result[0][0].value).toBe(1);
+    expect(result[0][1].value).toBe(3);
+    expect(result[0][2].value).toBe(5);
+
+    expect(result[1].length).toBe(2);
+    expect(result[1][0].id).toBe("5");
+    expect(result[1][1].id).toBe("6");
+  });
+});
